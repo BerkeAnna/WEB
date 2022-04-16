8c1a51dd03385e1ba7b9a5d36e45aa1d6b566aa2
BerkeAnna <berke.anna0803@gmail.com>
2022-04-16 17:41:10 +0200

    klj

---
+++ app/src/main/java/com/example/webshop/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.webshop;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText userNameEditText;
+    EditText passwordEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userNameEditText = findViewById(R.id.edittext_username);
+        passwordEditText = findViewById(R.id.edittext_password);
+    }
+
+    public void bej(View view) {
+        String userName = userNameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", jelszo: " + password );
+    }
+}
