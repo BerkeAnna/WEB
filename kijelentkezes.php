00657fcc52a5da24983a6a92b7511d8841313458
BerkeAnna <berke.anna0803@gmail.com>
2022-05-26 12:45:38 +0200

    delete

--- kijelentkezes.php
+++
@@ -1,7 +0,0 @@
-<?php
- // session_start();
-  session_unset();          //ures lesz a $_SESSION tomb
-  session_destroy();        //munkamenet törlése
-
-  header("Location: bejelentkezes.php");    // átirányít a bejelentkezes.phpra
-?>
